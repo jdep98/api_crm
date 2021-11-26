b46a1cf00ae3a942a2cfd3982957f77796208b9d
Juan Pablo De la torre Valdez <ptorre@creativaweb.mx>
2021-11-26 13:47:02 -0600

    <Link> Finalizado

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,19 +1,18 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, Link } from 'react-router-dom'
 const Layout = () => {
     return (
         <div className="md:flex md:min-h-screen">
-
             <div className="md:w-1/4 bg-blue-900 px-5 py-10">
                 <h2 className="text-4xl font-black text-center text-white ">CRM - Clientes</h2>
                 <nav className="mt-10">
-                    <a 
+                    <Link 
                         className="text-white text-2xl block mt-2 hover:text-blue-300"
-                        href="/clientes"
-                    >Clientes</a>
-                    <a 
+                        to="/clientes"
+                    >Clientes</Link>
+                    <Link 
                         className="text-white text-2xl block mt-2 hover:text-blue-300"
-                        href="/clientes/nuevo"
-                    >Nuevo Cliente</a>
+                        to="/clientes/nuevo"
+                    >Nuevo Cliente</Link>
                 </nav>
             </div>
 
