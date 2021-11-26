ca548f102ab26ff2acf9a341123cf5705d7b307c
Juan Pablo De la torre Valdez <ptorre@creativaweb.mx>
2021-11-26 16:47:41 -0600

    formulario finalizado

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -22,7 +22,7 @@ const Layout = () => {
                 </nav>
             </div>
 
-            <div className="md:w-3/4 p-10">
+            <div className="md:w-3/4 p-10 md:h-screen overflow-scroll">
                 <Outlet />
             </div>
             
