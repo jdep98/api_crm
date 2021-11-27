024d4abd662aef57443d81da4287fe837dfbe1df
Juan Pablo De la torre Valdez <ptorre@creativaweb.mx>
2021-11-27 16:29:03 -0600

    detectando id de usuario para ver info

---
+++ src/paginas/VerCliente.jsx
@@ -0,0 +1,15 @@
+import { useParams } from 'react-router-dom'
+
+const VerCliente = () => {
+
+    const { id } = useParams()
+
+    console.log(id)
+    return (
+        <div>
+            <h1>VerCliente.jsx</h1>
+        </div>
+    )
+}
+
+export default VerCliente
